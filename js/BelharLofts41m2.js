8ad4f81bdf901ac26fc070d9a232ef108b07262b
jarredc23 <131195538+jarredc23@users.noreply.github.com>
2023-08-17 19:50:22 +0200

    listings Updated
    update the listings field with all the JS connected and all the Photos for each unit implemented!

--- js/BelharLofts41m2.js
+++ js/BelharLofts41m2.js
@@ -7,9 +7,20 @@ const apartmentLofts41 = {
   price: "R 746,000.00",
   status: "avadibal",
   undertext: "insert text",
-  imageUrl1: "house-1.jpg",
+  imageUrl1: "assets/images/41m2 Phase 3 Belhar Lofts/front img.jpeg",
   SquareMeter: "41㎡",
   undertext: "insert text",
+  front_discription: "Get your stunning new modan house today, dont think twice about your familys futher!",
+  slideImg01: "assets/images/41m2 Phase 3 Belhar Lofts/1.jpeg",
+  slideImg02: "assets/images/41m2 Phase 3 Belhar Lofts/2.jpeg",
+  slideImg03: "assets/images/41m2 Phase 3 Belhar Lofts/3.jpeg",
+  slideImg04: "assets/images/41m2 Phase 3 Belhar Lofts/4.jpeg",
+  slideImg05: "assets/images/41m2 Phase 3 Belhar Lofts/5.jpeg",
+  slideImg06: "assets/images/41m2 Phase 3 Belhar Lofts/6.jpeg",
+  slideImg07: "assets/images/41m2 Phase 3 Belhar Lofts/7.jpeg",
+  slideImg08: "assets/images/41m2 Phase 3 Belhar Lofts/8.jpeg",
+  slideImg09: "assets/images/41m2 Phase 3 Belhar Lofts/9.jpeg",
+  slideImg10: "assets/images/41m2 Phase 3 Belhar Lofts/10.jpeg",
 
   changeStatusPending: function () {
     this.status = "Low Amount left";
@@ -20,9 +31,49 @@ const apartmentLofts41 = {
   },
 };
 
+
+const house4 = apartmentLofts41.price;
+
+function populateImage_apartmentLofts41m2() {
+  document.getElementById("img1").src = apartmentLofts41.slideImg01;
+  document.getElementById("img2").src = apartmentLofts41.slideImg02;
+  document.getElementById("img3").src = apartmentLofts41.slideImg03;
+  document.getElementById("img4").src = apartmentLofts41.slideImg04;
+  document.getElementById("img5").src = apartmentLofts41.slideImg05;
+  document.getElementById("img6").src = apartmentLofts41.slideImg06;
+  document.getElementById("img7").src = apartmentLofts41.slideImg07;
+  document.getElementById("img8").src = apartmentLofts41.slideImg08;
+  document.getElementById("img9").src = apartmentLofts41.slideImg09;
+  document.getElementById("img10").src = apartmentLofts41.slideImg10;
+
+}
+
+
+
 document.getElementById("houseImg4").src = apartmentLofts41.imageUrl1;
 document.getElementById("nameLofts41").innerHTML = apartmentLofts41.name;
 document.getElementById("priceLofts41").innerHTML = apartmentLofts41.price;
 document.getElementById("addressLofts41").innerHTML = apartmentLofts41.address1;
-document.querySelector(".frontDiscription41").innerHTML =
-  apartmentLofts41.undertext;
+document.querySelector(".frontDiscription41").innerHTML = apartmentLofts41.undertext;
+
+
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+//export function
+
+function getIds() {
+  document.getElementById("inputPurchasePrice").value = apartmentLofts41.price;
+  console.log(apartmentLofts41.price);
+}
--- js/DeGoedeHoop.js
+++ js/DeGoedeHoop.js
@@ -9,6 +9,18 @@ const DegoedeHoop = {
   imageUrl: "house-1 copy.jpg",
   SquareMeter: "40㎡",
   undertext: "insert text",
+  slideImg1: "assets/images/De Goede Hoop/1.png",
+  slideImg2: "assets/images/De Goede Hoop/2.png",
+  slideImg3: "assets/images/De Goede Hoop/3.png",
+  slideImg4: "assets/images/De Goede Hoop/4.png",
+  slideImg5: "assets/images/De Goede Hoop/5.png",
+  slideImg6: "assets/images/De Goede Hoop/6.jpg",
+  slideImg7: "assets/images/De Goede Hoop/7.jpg",
+  slideImg8: "assets/images/De Goede Hoop/8.jpg",
+  slideImg9: "assets/images/De Goede Hoop/9.jpg",
+  slideImg10: "assets/images/De Goede Hoop/10.jpg",
+
+  
 
   changeStatusPending: function () {
     this.status = "Low Amount Left";
@@ -18,9 +30,63 @@ const DegoedeHoop = {
     this.status = "Sold Out";
   },
 };
+const house5 = DegoedeHoop.price;
+
+
+
+
+function populateImage_DeGoedeHoop() {
+  document.getElementById("img1").src = DegoedeHoop.slideImg1;
+  document.getElementById("img2").src = DegoedeHoop.slideImg2;
+  document.getElementById("img3").src = DegoedeHoop.slideImg3;
+  document.getElementById("img4").src = DegoedeHoop.slideImg4;
+  document.getElementById("img5").src = DegoedeHoop.slideImg5;
+  document.getElementById("img6").src = DegoedeHoop.slideImg6;
+  document.getElementById("img7").src = DegoedeHoop.slideImg7;
+  document.getElementById("img8").src = DegoedeHoop.slideImg8;
+  document.getElementById("img9").src = DegoedeHoop.slideImg9;
+  document.getElementById("img10").src = DegoedeHoop.slideImg10;
+
+
+}
 
 document.getElementById("houseImg5").src = DegoedeHoop.imageUrl;
 document.getElementById("hoopName").innerHTML = DegoedeHoop.name;
 document.getElementById("hoopPrice").innerHTML = DegoedeHoop.price;
 document.getElementById("hoopAddress").innerHTML = DegoedeHoop.address;
 document.querySelector(".HoopDisc").innerHTML = DegoedeHoop.undertext;
+
+
+
+
+
+//Photos//
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+//export function
+
+function getIds() {
+  document.getElementById("inputPurchasePrice").value = DegoedeHoop.price;
+  console.log(DegoedeHoop.price);
+}
+
